a384cdc71d61f30a3df6b31f4f90667ab0708dcc
so1n <qaz6803609@163.com>
2018-02-04 23:34:35 +0800

    update

--- blogproject/online_status/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import OnlineStatus
-# Register your models here.
-
-admin.site.register(OnlineStatus)
--- blogproject/online_status/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OnlineStatusConfig(AppConfig):
-    name = 'online_status'
